1471c13d0ecff099f9347ea0373ec4de0af9c1a7
Sumit Sharma <sumitsharmaskr26@gmail.com>
2021-03-07 11:54:16 +0530

    BrainRefresher App

---
+++ app/src/main/java/com/example/brainrefresher/MainActivity.java
@@ -0,0 +1,154 @@
+package com.example.brainrefresher;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Vibrator;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button goButton;
+    Vibrator v;
+    ArrayList<Integer> answers = new ArrayList<Integer>();
+    int locationOfCorrectAnswer;
+    TextView resultTextView;
+    int score = 0;
+    int numberOfQuestions = 0;
+    TextView scoreTextView;
+    Button button0;
+    Button button1;
+    Button button2;
+    Button button3;
+    TextView sumTextView;
+    TextView timerTextView;
+    Button playAgainButton;
+    ConstraintLayout gameLayout;
+
+
+
+    @SuppressLint("SetTextI18n")
+    public void playAgain(View view) {
+        score = 0;
+        numberOfQuestions = 0;
+        timerTextView.setText("30s");
+        scoreTextView.setText(score +"/"+Integer.toString(numberOfQuestions));
+        newQuestion();
+        playAgainButton.setVisibility(View.INVISIBLE);
+        button0.setVisibility(View.VISIBLE);
+        button1.setVisibility(View.VISIBLE);
+        button2.setVisibility(View.VISIBLE);
+        button3.setVisibility(View.VISIBLE);
+        sumTextView.setVisibility(View.VISIBLE);
+
+        resultTextView.setText("");
+
+        new CountDownTimer(30100,1000) {
+
+            @Override
+            public void onTick(long l) {
+                timerTextView.setText(String.valueOf(l / 1000) + "s");
+            }
+
+            @Override
+            public void onFinish() {
+                resultTextView.setText("Done 🤟");
+                playAgainButton.setVisibility(View.VISIBLE);
+                sumTextView.setVisibility(View.INVISIBLE);
+                button0.setVisibility(View.INVISIBLE);
+                button1.setVisibility(View.INVISIBLE);
+                button2.setVisibility(View.INVISIBLE);
+                button3.setVisibility(View.INVISIBLE);
+            }
+        }.start();
+    }
+
+    public void chooseAnswer(View view) {
+        if (Integer.toString(locationOfCorrectAnswer).equals(view.getTag().toString())) {
+            resultTextView.setText("Correct 😃");
+            score++;
+        } else {
+            resultTextView.setText("Wrong 😢");
+        }
+        numberOfQuestions++;
+        scoreTextView.setText(Integer.toString(score)+"/"+Integer.toString(numberOfQuestions));
+        newQuestion();
+        v.vibrate(100);
+
+    }
+
+    public void start(View view) {
+        v.vibrate(100);
+        goButton.setVisibility(View.INVISIBLE);
+        gameLayout.setVisibility(View.VISIBLE);
+        playAgain(findViewById(R.id.timerTextView));
+    }
+
+    public void newQuestion() {
+        Random rand = new Random();
+
+        int a = rand.nextInt(21);
+        int b = rand.nextInt(21);
+
+        sumTextView.setText(Integer.toString(a) + " + " + Integer.toString(b));
+
+        locationOfCorrectAnswer = rand.nextInt(4);
+
+        answers.clear();
+
+        for (int i=0; i<4; i++) {
+            if (i == locationOfCorrectAnswer) {
+                answers.add(a+b);
+            } else {
+                int wrongAnswer = rand.nextInt(41);
+
+                while (wrongAnswer == a+b) {
+                    wrongAnswer = rand.nextInt(41);
+                }
+
+                answers.add(wrongAnswer);
+            }
+
+        }
+
+        v.vibrate(100);
+        button0.setText(Integer.toString(answers.get(0)));
+        button1.setText(Integer.toString(answers.get(1)));
+        button2.setText(Integer.toString(answers.get(2)));
+        button3.setText(Integer.toString(answers.get(3)));
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sumTextView = findViewById(R.id.sumTextView);
+        button0 = findViewById(R.id.button0);
+        button1 = findViewById(R.id.button1);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        resultTextView = findViewById(R.id.resultTextView);
+        scoreTextView = findViewById(R.id.scoreTextView);
+        timerTextView = findViewById(R.id.timerTextView);
+        playAgainButton = findViewById(R.id.playAgainButton);
+        gameLayout = findViewById(R.id.gameLayout);
+        goButton = findViewById(R.id.goButton);
+        v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+
+        goButton.setVisibility(View.VISIBLE);
+        gameLayout.setVisibility(View.INVISIBLE);
+
+    }
+}
---
+++ app/src/main/java/com/example/brainrefresher/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.example.brainrefresher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 5000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+
+        //Splash Screen Code to call new Activity after some time
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
